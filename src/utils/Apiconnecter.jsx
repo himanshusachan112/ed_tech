f1eda4e4b78d93ed6e10c27f269534967eb6e7e2
Himanshu <himanshusachan150@gmail.com>
2026-03-22 10:30:58 +0530

    new

--- src/utils/Apiconnecter.jsx
+++ src/utils/Apiconnecter.jsx
@@ -1,13 +1,27 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({});
+
+// export const apiConnector = (method, url, bodyData, headers, params) => {
+//   return axiosInstance({
+//     method: `${method}`,
+//     url: `${url}`,
+//     data: bodyData ? bodyData : null,
+//     headers: headers ? headers : null,
+//     params: params ? params : null,
+//   });
+// };
 import axios from "axios";
 
 export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
-  return axiosInstance({
-    method: `${method}`,
-    url: `${url}`,
-    data: bodyData ? bodyData : null,
-    headers: headers ? headers : null,
-    params: params ? params : null,
-  });
+    return axiosInstance({
+        method: method, // No need for template literals `${method}`
+        url: url,
+        data: bodyData || null,
+        // Check if bodyData is FormData. If so, don't force Content-Type.
+        headers: headers ? headers : undefined, 
+        params: params || null,
+    });
 };
